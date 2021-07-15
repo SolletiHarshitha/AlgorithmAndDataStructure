b3e1831a9757761bf2ea8c1242fcc95d19bbce2e
SolletiHarshitharigin <solletiharshitha@gmail.com>
2021-07-16 01:06:58 +0530

    [Harshitha] ALgorithm Problems

---
+++ AlgorithmPrograms/MergeSort.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    class MergeSort
+    {
+        public static void ReadInput()
+        {
+            Console.WriteLine("Enter size of the array : ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter elements of an array : ");
+            string[] arr = new string[size];
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = Console.ReadLine();
+            }
+            SortMethod(arr,0,size-1);
+            Console.WriteLine("The elements after performing Merge sort : ");
+            for(int i=0;i<size;i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+        }
+        private static void SortMethod(string[] arr,int left,int right)
+        {
+            int mid;
+            if (right > left)
+            {
+                mid = (right + left) / 2;
+                SortMethod(arr, left, mid);
+                SortMethod(arr, (mid + 1), right);
+                MergeMethod(arr, left, (mid + 1), right);
+            }
+        }
+        private static void MergeMethod(string[] arr,int left,int mid,int right)
+        {
+            string[] temp = new string[25];
+            int i, left_end, num_elements, tmp_pos;
+            left_end = (mid - 1);
+            tmp_pos = left;
+            num_elements = (right - left + 1);
+            while ((left <= left_end) && (mid <= right))
+            {
+                if ((arr[left].CompareTo(arr[mid]))<0)
+                    temp[tmp_pos++] = arr[left++];
+                else
+                    temp[tmp_pos++] = arr[mid++];
+            }
+            while (left <= left_end)
+                temp[tmp_pos++] = arr[left++];
+            while (mid <= right)
+                temp[tmp_pos++] = arr[mid++];
+            for (i = 0; i < num_elements; i++)
+            {
+                arr[right] = temp[right];
+                right--;
+            }
+        }
+    }
+}
