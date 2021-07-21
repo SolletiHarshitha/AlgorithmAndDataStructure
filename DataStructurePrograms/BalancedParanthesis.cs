a448d04fe195d35aa426d589917cc4f3a1dda2d4
solletiharshitha <solletiharshitha@gmail.com>
2021-07-22 02:14:38 +0530

    [Harshitha] Balanced Paranthesis

---
+++ DataStructurePrograms/BalancedParanthesis.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructurePrograms
+{
+    class BalancedParanthesis
+    {
+        public void ReadInput()
+        {
+
+            Stack<char> stack = new Stack<char>();
+            int balanced = 0;
+            string equation = "(5+6)∗(7+8)/(4+3)(5+6)∗(7+8)/(4+3)";
+            Console.WriteLine("Equation : \n" + equation);
+
+            for (int i = 0; i < equation.Length; i++)
+            {
+                if (equation[i].Equals('('))
+                {
+                    stack.Push(equation[i]);
+                }
+                else if (equation[i].Equals(')'))
+                {
+                    balanced = stack.pop();
+                    if (balanced == 1)
+                    {
+                        break;
+                    }
+                }
+            }
+            if (stack.Peek() == 1)
+            {
+                Console.WriteLine("The Equation is balanced");
+            }
+            else if (stack.Peek() == 0 && balanced == 0)
+            {
+                Console.WriteLine("The Equation is not balanced");
+            }
+        }
+    }
+
+}
+
--- DataStructurePrograms/Program.cs
+++ DataStructurePrograms/Program.cs
@@ -6,9 +6,32 @@ namespace DataStructurePrograms
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            UnorderedList unordered = new UnorderedList();
-            unordered.ReadInput();
+            Console.WriteLine("------------------DataStructure Programs-------------");
+            bool alive = true;
+            while (alive)
+            {
+                Console.WriteLine("\n1.Unordered List");
+                Console.WriteLine("2.Balanced Paranthesis");
+                Console.WriteLine("0.Exit");
+
+                Console.WriteLine("Choose an option : ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        UnorderedList unordered = new UnorderedList();
+                        unordered.ReadInput();
+                        break;
+                    case 2:
+                        BalancedParanthesis paranthesis = new BalancedParanthesis();
+                        paranthesis.ReadInput();
+                        break;
+                    case 0:
+                        alive = false;
+                        break;
+                }
+                
+            }
         }
     }
 }
